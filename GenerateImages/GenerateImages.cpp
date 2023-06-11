d734dcb9485f9f744c427d1fb8c4dd7921eed251
Diego Alejandro Sepulveda Huetio <108904111+HUETIO@users.noreply.github.com>
2023-06-10 21:16:44 -0500

    666

--- GenerateImages/GenerateImages.cpp
+++ GenerateImages/GenerateImages.cpp
@@ -1,6 +1,7 @@
 #include "ImageGenerator.h"
 #include <iostream>
-#include <limits> // Para utilizar std::numeric_limits
+#include <string>
+#include <limits>
 
 bool IsInteger(const std::string& str)
 {
@@ -14,59 +15,92 @@ bool IsInteger(const std::string& str)
     return true;
 }
 
-int main()
+bool IsValidResponse(const std::string& response)
 {
-    ImageGenerator imageGenerator;
-    // ruta de las imagenes aqui:
-    std::string imageFile1 = "../Resources/sino801_540x1200 (1).raw";
-    std::string imageFile2 = "../Resources/sino800_540x1200 (1).raw";
-    std::string differenceImageFile;
-    // ajustar tamano aqui:
-    int imageSize = 540 * 1200;
-    int imageCount;
-    // metodo validacion entrada
+    return (response == "Y" || response == "N");
+}
+
+char GetRepeatResponse()
+{
     std::string userInput;
     bool validInput = false;
 
     while (!validInput)
     {
-        std::cout << "Enter the number of images to generate (up to 5): ";
+        std::cout << "Do you want to repeat the program? (Y/N): ";
         std::cin >> userInput;
 
-        if (IsInteger(userInput))
+        if (IsValidResponse(userInput))
         {
-            imageCount = std::stoi(userInput);
-            if (imageCount >= 1 && imageCount <= 5)
-            {
-                validInput = true;
-            }
+            validInput = true;
+        }
+        else
+        {
+            std::cout << "Invalid response. Please enter 'Y' to repeat or 'N' to exit." << std::endl;
         }
 
-        if (!validInput)
-        {
-            std::cout << "Invalid input. Please enter a valid integer number between 1 and 5." << std::endl;
-        }
-
-        // Limpiar el estado del flujo de entrada y descartar cualquier entrada no válida
+        // Clear the input stream
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
-    for (int i = 1; i <= imageCount; ++i)
+    return userInput[0];
+}
+
+int main()
+{
+    char repeatProgram ='Y';
+
+    while (repeatProgram == 'Y')
     {
-        differenceImageFile = "ImageGenerate" + std::to_string(i) + ".raw";
+        ImageGenerator imageGenerator;
+        std::string imageFile1 = "../Resources/template.raw";
+        std::string imageFile2 = "../Resources/sino800_540x1200 (1).raw";
+        std::string differenceImageFile;
+        int imageSize = 540 * 1200;
+        int imageCount;
+        std::string userInput;
+        bool validInput = false;
 
-        // Leer las dos imágenes de entrada
-        std::vector<unsigned short> image1 = imageGenerator.ReadImageFile(imageFile1, imageSize);
-        std::vector<unsigned short> image2 = imageGenerator.ReadImageFile(imageFile2, imageSize);
+        while (!validInput)
+        {
+            std::cout << "Enter the number of images to generate (up to 5): ";
+            std::cin >> userInput;
 
-        // Generar la imagen de diferencia
-        std::vector<unsigned short> differenceImage = imageGenerator.GenerateDifferenceImage(image1, image2);
+            if (IsInteger(userInput))
+            {
+                imageCount = std::stoi(userInput);
+                if (imageCount >= 1 && imageCount <= 5)
+                {
+                    validInput = true;
+                }
+            }
 
-        // Guardar la imagen de diferencia en un archivo
-        imageGenerator.SaveGeneratedImage(differenceImageFile, differenceImage);
+            if (!validInput)
+            {
+                std::cout << "Invalid input. Please enter a valid integer number between 1 and 5." << std::endl;
+            }
 
-        std::cout << "Difference image " << i << " generated successfully." << std::endl;
+            // Clear the input stream
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+        for (int i = 1; i <= imageCount; ++i)
+        {
+            differenceImageFile = "ImageGenerate" + std::to_string(i) + ".raw";
+
+            std::vector<unsigned short> image1 = imageGenerator.ReadImageFile(imageFile1, imageSize);
+            std::vector<unsigned short> image2 = imageGenerator.ReadImageFile(imageFile2, imageSize);
+
+            std::vector<unsigned short> differenceImage = imageGenerator.GenerateDifferenceImage(image1, image2);
+
+            imageGenerator.SaveGeneratedImage(differenceImageFile, differenceImage);
+
+            std::cout << "Difference image " << i << " generated successfully." << std::endl;
+        }
+
+        repeatProgram = GetRepeatResponse();
     }
 
     return 0;
