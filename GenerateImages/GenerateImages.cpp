1cdac8bd927d256b026dad814b988ff6cef1cc42
Diego Alejandro Sepulveda Huetio <108904111+HUETIO@users.noreply.github.com>
2023-06-10 19:38:57 -0500

    Actualizacion

--- GenerateImages/GenerateImages.cpp
+++ GenerateImages/GenerateImages.cpp
@@ -1,23 +1,55 @@
 #include "ImageGenerator.h"
 #include <iostream>
+#include <limits> // Para utilizar std::numeric_limits
+
+bool IsInteger(const std::string& str)
+{
+    for (char c : str)
+    {
+        if (!std::isdigit(c))
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main()
 {
     ImageGenerator imageGenerator;
+    // ruta de las imagenes aqui:
     std::string imageFile1 = "../Resources/sino801_540x1200 (1).raw";
     std::string imageFile2 = "../Resources/sino800_540x1200 (1).raw";
     std::string differenceImageFile;
-
+    // ajustar tamano aqui:
     int imageSize = 540 * 1200;
     int imageCount;
+    // metodo validacion entrada
+    std::string userInput;
+    bool validInput = false;
 
-    std::cout << "Enter the number of images to generate (up to 5): ";
-    std::cin >> imageCount;
-
-    if (imageCount < 1 || imageCount > 5)
+    while (!validInput)
     {
-        std::cout << "Invalid image count. Please enter a number between 1 and 5." << std::endl;
-        return 1;
+        std::cout << "Enter the number of images to generate (up to 5): ";
+        std::cin >> userInput;
+
+        if (IsInteger(userInput))
+        {
+            imageCount = std::stoi(userInput);
+            if (imageCount >= 1 && imageCount <= 5)
+            {
+                validInput = true;
+            }
+        }
+
+        if (!validInput)
+        {
+            std::cout << "Invalid input. Please enter a valid integer number between 1 and 5." << std::endl;
+        }
+
+        // Limpiar el estado del flujo de entrada y descartar cualquier entrada no válida
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
     for (int i = 1; i <= imageCount; ++i)
